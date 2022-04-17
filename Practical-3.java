1ced3f25da57b76dad5bad7dac14113f48eb655a
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2022-04-17 11:00:36 +0530

    Create Practical-3.java

---
+++ Practical-3.java
@@ -0,0 +1,11 @@
+/*Write a program to sum two numbers. Here inputs are provided through command line argument.*/
+
+class Sum
+	{
+	    public static void main(String args[]){
+	      int x=Integer.parseInt(args[0]);
+	      int y=Integer.parseInt(args[1]);
+	      System.out.println(x+y);
+	    }
+	}
+
